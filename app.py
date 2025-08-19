bdafe92fa0f0bff868de753181c843ecb041263f
eugene <kapombek@gmail.com>
2025-08-19 12:06:18 +0300

    removed multiple cors header

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ api.add_resource(ReportStatusUpdateResource, "/reports/<int:report_id>/status")
 @app.after_request
 def after_request(response):
     # CORS headers
-    response.headers.add('Access-Control-Allow-Origin', BASE_URL)
+    response.headers.add('Access-Control-Allow-Origin')
     response.headers.add('Access-Control-Allow-Credentials', 'true')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-CSRF-Token')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
